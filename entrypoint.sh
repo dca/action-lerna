f0bed9c1c10e1f6e6a111d731d2d88931d8de644
Dca Hsu <dcahsu@gmail.com>
2020-06-16 17:46:29 +0800

    feat: Add npm config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_USEREMAIL
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPO
-# npm config set _auth ${{ secrets.NPM_AUTH_TOKEN }}
-# npm config set https-proxy "${{ secrets.PROXY_SERVER }}"
+npm config set _auth $NPM_AUTH_TOKEN
+npm config set https-proxy $NPM_PROXY_SERVER
 
 lerna bootstrap --no-ci
 lerna publish --conventional-commits --cd-version=$LERNA_LEVEL --yes
